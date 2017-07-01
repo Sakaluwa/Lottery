6997cc05dde5021ed54554f9d343303f43b689b9
冯立刚 <948391046@qq.com>
2017-07-02 00:51:41 +0800

    将选号结果打印到文本文件

--- lottery.py
+++ lottery.py
@@ -23,23 +23,34 @@ def random_non_repetitive_list(length, divisor):
     number.sort()
     return number
 
-count = int(input("请输入您的双色球彩票选取注数，按 ‘Enter’ 键结束："))
+f = open('彩票选号.txt', 'w')  # 新建选号结果存储文件
+f.close()
+count = input("请输入您的双色球彩票选取注数，按 ‘Enter’ 键结束 ：")
+f = open('彩票选号.txt', 'a')
+f.write('双色球选号 ' + count + ' 注：\n')
+count = int(count)
 while count > 0:
     red_number = random_non_repetitive_list(6, 33)  # 6 个红色球
-    blue_number = random_non_repetitive_list(1, 16) # 1 个蓝色球
-    print(red_number[0], ",", red_number[1], ",", red_number[2], ",",
-          red_number[3], ",", red_number[4], ",", red_number[5], ",",
-          blue_number[0])
+    blue_number = random_non_repetitive_list(1, 16)  # 1 个蓝色球
+    # 双色球选号
+    lottery_1 = str(red_number[0]) + "," + str(red_number[1]) + "," + str(red_number[2]) + "," + str(red_number[3]) \
+                + "," + str(red_number[4]) + "," + str(red_number[5]) + "," + str(blue_number[0]) + "\n"
+    f.write(lottery_1)
     count = count - 1
 
-count = int(input("请输入您的大乐透彩票选取注数，按 ‘Enter’ 键结束："))
+count = input(" \n 请输入您的大乐透彩票选取注数，按 ‘Enter’ 键结束 ：")
+f.write('大乐透选号 ' + count + ' 注：\n')
+count = int(count)
 while count > 0:
-    red_number = random_non_repetitive_list(5, 35) # 5 个蓝色球
-    blue_number = random_non_repetitive_list(2, 12) # 2 个红色球
-    print(red_number[0], ",", red_number[1], ",", red_number[2], ",",
-          red_number[3], ",", red_number[4], ",", blue_number[0], ",",
-          blue_number[1])
+    red_number = random_non_repetitive_list(5, 35)  # 5 个蓝色球
+    blue_number = random_non_repetitive_list(2, 12)  # 2 个红色球
+    # 大乐透选号
+    lottery_2 = str(red_number[0]) + "," + str(red_number[1]) + "," + str(red_number[2]) + "," + str(red_number[3]) \
+                + "," + str(red_number[4]) + "," + str(blue_number[0]) + "," + str(blue_number[1]) + "\n"
+    f.write(lottery_2)
     count = count - 1
+
+f.close()
 
 
 
